10136c1d49a0e443687d27ed36f6f520767bb11b
ColdAlgorithem <maticpv@gmail.com>
2025-04-18 15:04:01 +0200

    Added NCA

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import time
 from DP.NCA.gen_heightmaps import gen_hightmap
 start = time.time()
-uploaded_file = gen_hightmap("Perlin")
+uploaded_file = gen_hightmap("Noise Perlin")
 end = time.time()
 print(end - start)
 
